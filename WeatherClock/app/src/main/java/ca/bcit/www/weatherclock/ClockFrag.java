615857e42ee4908d804890880e7bf361cce90560
lockan <lockan@gmail.com>
2014-10-16 16:48:18 -0700

    Clock added with some basic formatting.

--- WeatherClock/app/src/main/java/ca/bcit/www/weatherclock/ClockFrag.java
+++ WeatherClock/app/src/main/java/ca/bcit/www/weatherclock/ClockFrag.java
@@ -1,19 +1,29 @@
 package ca.bcit.www.weatherclock;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextClock;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class ClockFrag extends android.support.v4.app.Fragment {
+
+    private TextClock clock;
 
     public ClockFrag() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-        Bundle savedInstanceState) {
+                             Bundle savedInstanceState) {
         View myFragmentView = inflater.inflate(R.layout.fragment_clock, container, false);
+        clock = (TextClock) getActivity().findViewById(R.id.textClock);
         return myFragmentView;
     }
 
